376bf45a44f632f3973f650b00d77facb5d16e35
dengshuai <dengshuai@njmyz.cn>
2025-09-05 11:00:11 +0800

    feat - 推送测试

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
